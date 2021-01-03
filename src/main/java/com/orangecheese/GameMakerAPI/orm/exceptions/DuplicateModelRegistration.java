f2601475553c7120873b2e33e4174db69047b8d2
Nosaiii <jcv.vanderlee@student.avans.nl>
2021-01-03 11:04:52 +0100

    Improved exception handling on ORM

--- src/main/java/com/orangecheese/GameMakerAPI/orm/exceptions/DuplicateModelRegistration.java
+++ src/main/java/com/orangecheese/GameMakerAPI/orm/exceptions/DuplicateModelRegistration.java
@@ -1,7 +1,9 @@
 package com.orangecheese.GameMakerAPI.orm.exceptions;
 
+import com.orangecheese.GameMakerAPI.orm.model.Model;
+
 public class DuplicateModelRegistration extends Exception {
-    public DuplicateModelRegistration() {
-        super("Duplicate model registration attempted in the model facade");
+    public DuplicateModelRegistration(Class<? extends Model> modelClass) {
+        super("An already-existing model was attempted to be registered (model: [" + modelClass.getSimpleName() + "])");
     }
 }
--- src/main/java/com/orangecheese/GameMakerAPI/orm/modelfacade/IModelFactory.java
+++ src/main/java/com/orangecheese/GameMakerAPI/orm/modelfacade/IModelFactory.java
@@ -1,10 +1,11 @@
 package com.orangecheese.GameMakerAPI.orm.modelfacade;
 
 import com.orangecheese.GameMakerAPI.orm.Query;
+import com.orangecheese.GameMakerAPI.orm.exceptions.MissingModelConstructor;
 import com.orangecheese.GameMakerAPI.orm.model.Model;
 
 public interface IModelFactory<T extends Model> {
     String buildQuery();
     Query<T> all();
-    T create();
+    T create() throws MissingModelConstructor;
 }
